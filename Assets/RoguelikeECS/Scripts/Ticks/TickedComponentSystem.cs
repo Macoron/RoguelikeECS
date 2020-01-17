000965d73927fc9d96c1e5f865be87a46ffa6f76
Alex Evgrashin <aevgrashin@yandex.ru>
2020-01-18 01:37:53 +0300

    Trying to implement ticks system

---
+++ Assets/RoguelikeECS/Scripts/Ticks/TickedComponentSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public abstract class TickedComponentSystem : ComponentSystem, ITickable
+{
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+    }
+
+    protected override void OnUpdate()
+    {
+        // usualy - do nothing, we care only about ticks
+    }
+
+    public abstract void OnTick();
+}
---
+++ Assets/RoguelikeECS/Scripts/Ticks/TickedController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickedController
+{
+    private static List<ITickable> allTickable = new List<ITickable>();
+    public static List<ITickable> AllTickable { get => allTickable; }
+
+    public static void RegisterTickable(ITickable tickable)
+    {
+        allTickable.Add(tickable);
+    }
+
+    public static void Tick()
+    {
+        foreach (var tickable in allTickable)
+            tickable.OnTick();
+    }
+
+}
